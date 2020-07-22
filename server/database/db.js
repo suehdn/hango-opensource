43673c696c8fab1086b030210d77e4c8c5209452
박우림 <woorimprog@gmail.com>
2020-07-22 16:12:05 +0900

    DB 설명을 위한 주석 추가
    Signed-off-by: 박우림 <woorimprog@gmail.com>

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,9 @@
 /* database/db.js */
+
+// mysql 모듈 포함
 const mysql = require('mysql');
 
+// mysql 연결을 위한 인스턴스 생성
 const db = mysql.createConnection({
   host: '', // DB 서버 도메인
   user: 'woorimprog', // 지정한 유저 아이디
@@ -8,6 +11,8 @@ const db = mysql.createConnection({
   database: 'sensing-dbserver' // 데이터 베이스 이름
 });
 
+// 연결 요청
 db.connect();
 
+// 외부와 연결시키기 위해 exports
 module.exports = db;
