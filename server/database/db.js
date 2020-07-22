ee47150450ed5a47ba9a3d616bf2bd9e61b71c25
박우림 <woorimprog@gmail.com>
2020-07-22 16:51:47 +0900

    서버 환경 변수를 이용한 DB 보안 설정
    1. db.js 수정
     (1) 클라우드 서버의 환경변수 추가
     (2) DB 소스에서 추가된 환경변수를 불러옴
       - process.env.*
     (3) 직접적으로 DB서버의 도메인, 관리자ID, 패스워드, DB명을 노출시키지 않음으로써 후에 발생할 보안 문제 대비
    
    Signed-off-by: 박우림 <woorimprog@gmail.com>

--- server/database/db.js
+++ server/database/db.js
@@ -5,10 +5,10 @@ const mysql = require('mysql');
 
 // mysql 연결을 위한 인스턴스 생성
 const db = mysql.createConnection({
-  host: '', // DB 서버 도메인
-  user: 'woorimprog', // 지정한 유저 아이디
-  password: '', //  지정한 패스워드
-  database: 'sensing-dbserver' // 데이터 베이스 이름
+  host: process.env.DB_DOMAIN,          // DB 서버 도메인
+  user: process.env.DB_USER,            // 지정한 유저 아이디
+  password: process.env.DB_PASSWORD,    //  지정한 패스워드
+  database: process.env.DB_NAME         // 데이터 베이스 이름
 });
 
 // 연결 요청
